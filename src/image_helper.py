945c94a66969b585d7a4e3e734f888c42f2a28fe
Preetham.Armoor <preetham.armoor@gmail.com>
2022-06-06 14:44:03 +0200

    unit test is clear

--- src/image_helper.py
+++ src/image_helper.py
@@ -7,9 +7,11 @@ def get_image(history, **kargs) -> str:
     :return: str containing file path
     """
     history.set_image_detaails(**kargs)
-
-    if os.path.exists(kargs["image_folder"]):
+    if os.path.exists(kargs["image_folder"]) is True:
         os.chdir(kargs["image_folder"])
         filename = open("test_image.png", "wb")
         filename.close()
+        os.chdir("..")
+    else:
+        raise ValueError("Invalid image_folder")
     return history.get_image_detaails()
